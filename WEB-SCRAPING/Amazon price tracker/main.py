2cb8f13ea97a1afc11db80cc4a41728a526951a7
Camille <kamile10za@gmail.com>
2026-08-10 12:32:48 +0300

    Update main.py

--- WEB-SCRAPING/Amazon price tracker/main.py
+++ WEB-SCRAPING/Amazon price tracker/main.py
@@ -12,7 +12,23 @@ YOUR_PASSWORD = os.getenv("YOUR_PASSWORD")
 print(repr(YOUR_SMTP_ADDRESS))
 
 url = "https://appbrewery.github.io/instant_pot/"
-response = requests.get(url)
+
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-GB,de;q=0.8,fr;q=0.6,en;q=0.4,ja;q=0.2",
+    "Dnt": "1",
+    "Priority": "u=1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Sec-Gpc": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
+}
+
+response = requests.get(url, headers=header)
 soup = BeautifulSoup(response.content, 'html.parser')
 price = soup.find(name="span", class_="a-offscreen")
 price = float(price.text[1:])
